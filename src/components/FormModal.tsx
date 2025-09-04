c9bb8100ff5820dd88acff97c5feddb1cb7ef8de
dumspice <dzunghoang04@gmail.com>
2025-09-04 17:33:38 +0700

    done form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const form: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 
 const FormModal = ({
   table,
@@ -45,8 +61,10 @@ const FormModal = ({
           Delete
         </button>
       </form>
+    ) : type === "create" || type === "update" ? (
+      form[table](type, data)
     ) : (
-      "Create or Update form"
+      "Form not found"
     );
   };
   return (
--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -35,7 +35,7 @@ const schema = z.object({
 
 type Inputs = z.infer<typeof schema>;
 
-const TeacherForm = ({
+const StudentForm = ({
   type,
   data,
 }: {
@@ -57,7 +57,7 @@ const TeacherForm = ({
   return (
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
       <h1 className="text-xl font-semibold">
-        {type === "create" ? "Create " : "Update "} a new teacher
+        {type === "create" ? "Create " : "Update "} a new student
       </h1>
       <span className="text-xs text-gray-400 font-medium">
         Authentication Information
@@ -179,4 +179,4 @@ const TeacherForm = ({
   );
 };
 
-export default TeacherForm;
+export default StudentForm;
