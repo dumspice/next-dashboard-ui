74d8e8ceb141726589f2e0e4610b67db7fe9aad0
dumspice <dzunghoang04@gmail.com>
2025-09-06 14:33:22 +0700

    add sign in page and authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
+  const role = user?.publicMetadata.role as string;
+
   return (
     <div className="flex items-center justify-between p-4">
       {/* Search bar */}
@@ -24,16 +29,21 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex flex-col">
-          <span className="text-sx leading-3 font-medium">John Doe</span>
-          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          <span className="text-sx leading-3 font-medium">
+            {user?.username}
+          </span>
+          <span className="text-[10px] text-gray-500 text-right">
+            {role.substring(0, 1).toUpperCase() + role.substring(1)}
+          </span>
         </div>
-        <Image
+        {/* <Image
           src="/avatar.png"
           alt=""
           width={36}
           height={36}
           className="rounded-full"
-        />
+        /> */}
+        <UserButton />
       </div>
     </div>
   );
